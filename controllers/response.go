b28e8eac9b034540025b55d62f8ee0c22b0dade9
aprian1337 <dwikyapriyan@gmail.com>
2022-12-25 01:24:18 +0700

    up

--- controllers/response.go
+++ controllers/response.go
@@ -18,7 +18,7 @@ type successResponse struct {
 
 func BaseErrorResponse(fctx *fiber.Ctx, err error, statusCode int) error {
 	return fctx.JSON(errorResponse{
-		ErrorMessage:     "Aplikasi sedang ada masalah, silakan coba lagi dalam waktu berkala.",
+		ErrorMessage:     "Aplikasi sedang ada masalah, silakan coba lagi dalam waktu dekat.",
 		DeveloperMessage: err.Error(),
 		Status:           statusCode,
 	})
